1d5a7830df655d5953048cb879e6ba5ca955e952
jmfp <therealjmfp@gmail.com>
2024-08-09 21:09:26 -0400

    fixed async await

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import parse from 'html-react-parser';
 import hljs from 'highlight.js/lib/core';
 import typescript from 'highlight.js/lib/languages/typescript';
-import ParallaxImage from "@/app/components/images/image";
+import { ParallaxHero } from "@/app/components/images/image";
 import Markdown from "react-markdown"
 import rehypeHighlight from "rehype-highlight"
 import 'highlight.js/styles/base16/pop.css'
@@ -30,15 +30,16 @@ export default async function Article({params}:{params: {slug: string}}){
     const post = await fetchPosts(params.slug)
     let content = parse(post.content)
     return(
-      <div>
-        <ParallaxImage 
+      <div className='display: flex h-full flex-col'>
+        <ParallaxHero image={post.image} height={"50"}/>
+        {/*<ParallaxImage 
           image={post.image}
           alt={post.slug}
           width={1920}
           height={500}
           style="w-[100%] h-[600px] rounded-lg m-[auto] object-cover border-4 border-violet-600"
           text=''
-        />
+        />*/}
           <div className="w-[100vw] content-center flex-auto p-10 prose-h1:text-violet-500">
           <h1 className="text-3xl font-extrabold text-center">{post.title}</h1>
             <div className="mt-24 prose m-[auto] prose-violet prose-xl dark:prose-invert prose-h2:text-violet-500 prose-li:color-violet-500">
