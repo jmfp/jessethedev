b0bba7c6ec1c166081a403ce1aae9b7303ac73e2
jmfp <therealjmfp@gmail.com>
2024-09-03 19:36:32 -0400

    auth

---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+export default function Services() {
+  return (
+    <div>
+        <h2>Fixed Price Services</h2>
+        <p>If all that you need is a simple website for your business that displays the location, business hours and contact information, I offer a fixed price solution
+            
+        </p>
+    </div>
+  )
+}
