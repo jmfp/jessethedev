b29c72127ff3bdc9b4fff8faeb1731e90d0a7ad1
jmfp <therealjmfp@gmail.com>
2024-09-08 22:53:29 -0400

    optimized images

--- app/page.tsx
+++ app/page.tsx
@@ -17,18 +17,18 @@ export default function Services() {
             {"I have over 10 years of experience in the field with a Bachelors degree in computer science. I like to take a laid back approach to communication, and a focused approach to work. "}
           </p>
         </div>
-        <ParallaxHero image="/images/respsocialsite.png" height={40}/>
-        <ParallaxHero image="/images/respwheel.png" height={40}/>
+        <ParallaxHero image="/images/social.webp" height={40}/>
+        <ParallaxHero image="/images/wheel.webp" height={40}/>
         <div className="m-16 text-center text-3xl">
         <p>{"I like to overdeliver and produce results that both parties can be proud of. With a focus on solving clients individual needs for each project, I enjoy creating a platform that will make you proud to share your content, services or products."}</p>
         </div>
         
-        <ParallaxHero image="/images/respsolo.png" height={40}/>
-        <ParallaxHero image="/images/resppoke.png" height={40}/>
+        <ParallaxHero image="/images/solo.webp" height={40}/>
+        <ParallaxHero image="/images/poke.webp" height={40}/>
         <div className="m-16 text-center text-3xl">
         <p>{"Offering backend, frontend, or fullstack custom services I am confident I have the ability to increase your businesses presence on the internet in whatever way you need."}</p>
         </div>
-        <ParallaxHero image="/images/me2.png" height={40}/>
+        <ParallaxHero image="/images/mespace.webp" height={40}/>
         <h2 className="m-auto text-primary text-3xl mt-6">Contact Me</h2>
       </div>
       <div className="md:hidden lg:hidden">
