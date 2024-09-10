f96a573477d9eaf9a66ddcacdc235687bff86bf0
jmfp <therealjmfp@gmail.com>
2024-09-10 05:19:11 -0400

    fixed mobile

--- app/page.tsx
+++ app/page.tsx
@@ -44,30 +44,35 @@ export default function Services() {
         <h2 className="m-auto text-primary text-3xl mt-6">Contact Me</h2>
       </div>
       <div className="md:hidden lg:hidden">
-      <h2 className="m-auto text-primary text-3xl">Web Design Services In Madison and Northeast Ohio</h2>
+        <Image 
+          src={"/images/hero.webp"}
+          alt="Web design madison ohio"
+          width={200}
+          height={200}
+        />
         <div className="m-16">
-          <h2 className="m-auto text-primary text-3xl">Why Me?</h2>
+        <h1 className="m-auto text-primary text-3xl">Web Design Services In Madison and Northeast Ohio</h1>
           <p className="m-auto text-3xl">
             {"I have over 10 years of experience in the field with a Bachelors degree in computer science. I like to take a laid back approach to communication, and a focused approach to work. "}
           </p>
         </div>
         <Image src={"/images/social.webp"}
-          width={800}
-          height={800}
+          width={200}
+          height={200}
           alt="socialsite" className="m-auto scale-150"/>
         <div className="m-16 text-center text-3xl">
         <p>{"I like to overdeliver and produce results that both parties can be proud of. With a focus on solving clients individual needs for each project, I enjoy creating a platform that will make you proud to share your content, services or products."}</p>
         </div>
         <Image src={"/images/solo.webp"}
-          width={800}
-          height={800}
+          width={200}
+          height={200}
           alt="socialsite" className="m-auto scale-150"/>
         <div className="m-16 text-center text-3xl">
         <p>{"Offering backend, frontend, or fullstack custom services I am confident I have the ability to increase your businesses presence on the internet in whatever way you need."}</p>
         </div>
         <Image src={"/images/wheel.webp"}
-          width={800}
-          height={800}
+          width={200}
+          height={200}
           alt="socialsite" className="m-auto scale-150"/>
         <h2 className="m-auto text-primary text-3xl">Contact Me</h2>
       </div>
